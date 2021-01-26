7e2ec95f78d5f517f108f92f675faae6c2f92780
ajwash <ajw53!njit.edu>
2021-01-26 17:22:08 -0500

    Add: added sound effects. Plays when the pong ball hits the borders and the paddles.

--- Pong.py
+++ Pong.py
@@ -12,7 +12,7 @@ score_a = 0
 score_b = 0
 
 # Paddle A
-paddle_a= turtle.Turtle()
+paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.color("lime green")
@@ -21,7 +21,7 @@ paddle_a.penup()
 paddle_a.goto(-350, 0)
 
 # Paddle B
-paddle_b= turtle.Turtle()
+paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
 paddle_b.color("lime green")
@@ -30,7 +30,7 @@ paddle_b.penup()
 paddle_b.goto(350, 0)
 
 # Ball
-ball= turtle.Turtle()
+ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
 ball.color("lime green")
@@ -48,26 +48,31 @@ pen.hideturtle()
 pen.goto(0, 260)
 pen.write("Player A: 0 Player B: 0", align="center", font=("Courier", 24, "normal"))
 
+
 # Function
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
     paddle_a.sety(y)
 
+
 def paddle_a_down():
     y = paddle_a.ycor()
     y -= 20
     paddle_a.sety(y)
+
 
 def paddle_b_up():
     y = paddle_b.ycor()
     y += 20
     paddle_b.sety(y)
 
+
 def paddle_b_down():
     y = paddle_b.ycor()
     y -= 20
     paddle_b.sety(y)
+
 
 # Keyword Binding
 wn.listen()
@@ -76,7 +81,7 @@ wn.onkeypress(paddle_a_down, "s")
 wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
 
-#Main Game Loop
+# Main Game Loop
 
 while True:
     wn.update()
@@ -85,15 +90,17 @@ while True:
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
-    #Border Checking
+    # Border Checking
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        os.system("afplay Bounce.wav&")
 
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        os.system("afplay Bounce.wav&")
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
@@ -107,14 +114,17 @@ while True:
         ball.dx *= -1
         score_b += 1
         pen.clear()
-        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align = "center", font = (
-        "Courier", 24, "normal"))
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=(
+            "Courier", 24, "normal"))
+        os.system("afplay Bounce.wav&")
 
     # Paddle and ball collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and (paddle_b.ycor() + 40 > ball.ycor() > paddle_b.ycor() - 50):
         ball.setx(340)
         ball.dx *= -1
+        os.system("afplay Bounce.wav&")
 
     if (ball.xcor() < -340 and ball.xcor() > -350) and (paddle_a.ycor() + -40 < ball.ycor() < paddle_a.ycor() - -50):
         ball.setx(-340)
         ball.dx *= -1
+        os.system("afplay Bounce.wav&")
